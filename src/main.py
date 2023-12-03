bd9b590e781b320c80f81c610f1b48d6785ff689
Felix Würsch <felix.wuersch@hm.edu>
2023-12-03 18:59:33 +0100

    basic pipeline cycle and example demonstration in main

--- src/main.py
+++ src/main.py
@@ -1,21 +1,131 @@
-# Import Modules
 import os
+
 from openai import OpenAI
+import requests
+from io import BytesIO
+from PIL import Image
+import utils
 
-client = OpenAI(
-    # defaults to os.environ.get("OPENAI_API_KEY")
-    api_key="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",
-)
+# Generate story using ChatGPT
+def generate_story(prompt):
+    client = OpenAI(
+        # defaults to os.environ.get("OPENAI_API_KEY")
+        api_key=utils.get_openai_api_key(),
+    )
 
-chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": "you're an ever changing picture frame, displayed in a musem - you initially present yourself with a real picture that's displayed in an actual museum in the world and give people an initial beginning of a story based in the picture which people can now add to. As people add and extend the initial story by writing a small continuation, you transform your picture into yet another artpiece that fits the stories current progression. You, as the ever changing picture, continue this forever. Start with providing the initial picture, ideally giving a link to it, and the beginning of the story. After every extension of your story you receive, show the new picture and the extended story which the user added to.",
-        }
-    ],
-    model="gpt-3.5-turbo",
-)
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ],
+        model="gpt-3.5-turbo",
+    )
+    story = chat_completion.choices[0].message.content
+    return story
 
-print(chat_completion.choices[0].message.content)
+def generate_description(story):
+    client = OpenAI(
+        # defaults to os.environ.get("OPENAI_API_KEY")
+        api_key=utils.get_openai_api_key(),
+    )
 
+    # Instruct ChatGPT to summarize the provided story
+    instruction = "Please summarize the following story into instructions for a image generation ai to create a fitting picture for it, suitable as a prompt used in another api call:\n"
+    prompt_with_instruction = f"{instruction}{story}"
+
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": prompt_with_instruction,
+            }
+        ],
+        model="gpt-3.5-turbo",
+    )
+
+    # Extract the generated description from the response
+    description = chat_completion.choices[0].message.content
+    return description
+
+# Create an image based on the story
+def generate_image(description, model="dall-e-3"):
+    client = OpenAI(
+        # defaults to os.environ.get("OPENAI_API_KEY")
+        api_key=utils.get_openai_api_key(),
+    )
+
+    response = client.images.generate(
+        model="dall-e-3",
+        prompt=description,
+        size="1024x1024",
+        quality="standard",
+        n=1,
+    )
+
+    image_url = response.data[0].url
+
+    # Download the image from the URL
+    image_response = requests.get(image_url)
+    image_data = BytesIO(image_response.content)
+
+    # Open and display the image
+    image = Image.open(image_data)
+    image.show()
+
+    return image
+
+
+
+def save_to_gallery(image, image_number):
+    # Get the current directory of main.py
+    current_directory = os.path.dirname(os.path.realpath(__file__))
+
+    # Navigate to the parent directory (root_folder)
+    parent_directory = os.path.abspath(os.path.join(current_directory, os.pardir))
+
+    # Create the path for the "gallery" folder in the parent directory
+    gallery_path = os.path.join(parent_directory, "gallery")
+
+    # Check if the "gallery" folder exists, and create it if not
+    if not os.path.exists(gallery_path):
+        os.makedirs(gallery_path)
+
+    image_filename = f"image_{image_number}.png"
+
+    # Your image file name with an embedded number
+    image_filename = f"image_{image_number}.png"
+
+    # Full path to save the image
+    image_path = os.path.join(gallery_path, image_filename)
+
+    # Save the image at the specified path
+    image.save(image_path)
+
+
+# Main function
+def main():
+
+    prompt = utils.get_preprompt()
+    story = 'As the sun began to set, a sense of unease settled over the sleepy town. From the shadows emerged a figure, moving silently through the deserted streets, its intentions unknown.'#generate_story(prompt)
+    print(story)
+
+    generated_description = generate_description(story)
+    print(generated_description)
+
+    image = generate_image(generated_description)
+    save_to_gallery(image, 1)
+
+    addition = 'Then, from behind the shadowy figure, a vehicle slowly crept itself through the streets, moving towards the figure as if it was tracking him - makin the figure panick and run away.'
+    continued_story = f"{story}{addition}"
+    print(continued_story)
+
+    generated_description2 = generate_description(continued_story)
+    print(generated_description2)
+
+    image = generate_image(generated_description2)
+    save_to_gallery(image, 2)
+
+if __name__ == "__main__":
+    main()
---
+++ src/utils.py
@@ -0,0 +1,34 @@
+import os
+
+def get_openai_api_key():
+    # Get the current directory of the main.py file
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+
+    # Navigate to the "data" folder from the "src" directory
+    data_directory = os.path.join(current_directory, '..', 'data')
+
+    # Construct the path to the api_key.txt file
+    api_key_path = os.path.join(data_directory, 'api_key.txt')
+
+    # Read the API key from the file
+    with open(api_key_path, 'r') as file:
+        api_key = file.read().strip()
+
+    return api_key
+
+def get_preprompt():
+    # Get the current directory of the main.py file
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+
+    # Navigate to the "data" folder from the "src" directory
+    data_directory = os.path.join(current_directory, '..', 'data')
+
+    # Construct the path to the api_key.txt file
+    preprompt_path = os.path.join(data_directory, 'preprompt.txt')
+
+    # Read the API key from the file
+    with open(preprompt_path, 'r') as file:
+        preprompt = file.read().strip()
+
+    return preprompt
+
